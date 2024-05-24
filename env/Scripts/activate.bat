3900b837d949e249b813198015a7bd121313134a
Roushan Mondal <roushan.mondal3@gmail.com>
2024-05-24 23:38:20 +0530

    Created cart functionalities and checkout

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\New folder\book-shop-ecommerce\env
+set VIRTUAL_ENV=D:\finalProject\env
 
 if not defined PROMPT set PROMPT=$P$G
 
