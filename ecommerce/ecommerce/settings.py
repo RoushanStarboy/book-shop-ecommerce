2db339bfd5ebdf8f49e4dcc4e5e505039e7fb9b6
Rahul Roy <140826511+RahulRoyBXT@users.noreply.github.com>
2024-08-17 04:49:41 +0530

    Update settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-49%_cfhpzmzaf6yemb$9$o^wnnyr@5+wouov5!(riy_abag(#('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
